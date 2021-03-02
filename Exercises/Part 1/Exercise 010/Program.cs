414a1658fb5859db5306e90bc72f8b1d6a5be976
conky-1 <petri.sipinen@centria.fi>
2021-03-02 18:35:56 +0200

    2.3.2021

--- Exercises/Part 1/Exercise 010/Program.cs
+++ Exercises/Part 1/Exercise 010/Program.cs
@@ -6,8 +6,19 @@ namespace exercise_10
     {
         public static void Main(string[] args)
         {
-          
-
+            Console.WriteLine("I will tell a story, but I need some information.");
+            Console.Write("Give a name for main character: ");
+            string name = Console.ReadLine();
+            Console.Write("Give the character a profession: ");
+            string profession = Console.ReadLine();
+            Console.WriteLine("Here is the story:\n" + 
+            "Once upon a time there was a " + profession + " called " + name +"\n"+
+            "On her way to work, " + name + " often pondered what being " +profession+ " meant to them.\n" +
+            "When you work as a " + profession + " you meet interesting people.\n" +
+             name + " enjoys their work as " + profession + ", The end");
+            Console.ReadLine();
+            
+                
 
         }
     }
