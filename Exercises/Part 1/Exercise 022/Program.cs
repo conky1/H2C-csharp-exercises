27b0c5d5007d11c99283cb6c33cf4632af269abd
conky-1 <petri.sipinen@centria.fi>
2021-03-08 18:35:06 +0200

    8.3.2021

--- Exercises/Part 1/Exercise 022/Program.cs
+++ Exercises/Part 1/Exercise 022/Program.cs
@@ -8,7 +8,16 @@ namespace exercise_22
     {
 
       // Write your code here:
-
+        
+        
+        Console.Write("Give number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Give second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Give second number: ");
+        double num3 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("The average is: " + (num1 + num2 + num3)/3);
+        Console.ReadLine();
     }
   }
 }
--- Exercises/Part 1/Exercise 023/Program.cs
+++ Exercises/Part 1/Exercise 023/Program.cs
@@ -8,6 +8,17 @@ namespace exercise_23
     {
 
       // Write your code here:
+        Console.Write("Give number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Give second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine();
+        Console.WriteLine(num1 + " + " + num2 + " = " + (num1 + num2));
+        Console.WriteLine(num1 + " - " + num2 + " = " + (num1 - num2));
+        Console.WriteLine(num1 + " * " + num2 + " = " + (num1 * num2));
+        Console.WriteLine(num1 + " / " + num2 + " = " + (num1 / num2));
+        Console.ReadLine();
+        
 
     }
   }
