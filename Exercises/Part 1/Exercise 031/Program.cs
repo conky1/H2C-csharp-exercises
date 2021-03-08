af728a16e3f6130edca03042c29693260ce8d888
conky-1 <petri.sipinen@centria.fi>
2021-03-08 23:48:58 +0200

    8.3.2021

--- Exercises/Part 1/Exercise 031/Program.cs
+++ Exercises/Part 1/Exercise 031/Program.cs
@@ -7,7 +7,16 @@ namespace exercise_31
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.Write("Give number: ");
+      int num = Convert.ToInt32(Console.ReadLine());
+      if((num % 2) == 0){
+            Console.WriteLine("Number is even!");
 
+            }
+      else{
+            Console.WriteLine("Number is odd!");
+            }
+      Console.ReadLine();
     }
   }
 }
