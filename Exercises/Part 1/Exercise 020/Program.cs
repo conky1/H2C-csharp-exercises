f6957e1cf9454c829fca877d8a4556313969b1c9
conky-1 <petri.sipinen@centria.fi>
2021-03-08 17:31:11 +0200

    8.3.2021

--- Exercises/Part 1/Exercise 020/Program.cs
+++ Exercises/Part 1/Exercise 020/Program.cs
@@ -8,7 +8,12 @@ namespace exercise_20
     {
 
       // Write your code here:
-
+        Console.Write("Give number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Give second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(num1 + " * " + num2 + " = " + (num1 * num2));
+        Console.ReadLine();
     }
   }
 }
