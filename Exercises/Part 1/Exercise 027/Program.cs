69c1ee6c3dbd2fd46f5cbe9caae003ef102ab8d7
conky-1 <petri.sipinen@centria.fi>
2021-03-08 21:59:55 +0200

    8.3.2021

--- Exercises/Part 1/Exercise 027/Program.cs
+++ Exercises/Part 1/Exercise 027/Program.cs
@@ -7,7 +7,19 @@ namespace exercise_27
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.Write("Give a number: ");
+      double num = Convert.ToDouble(Console.ReadLine());
+      
+      if (num < 0){
+            
+            Console.WriteLine("Value is negative.");
+     
+                    }
+      else{
+            Console.WriteLine("Value is positive.");
+            }
 
+      Console.ReadLine();
     }
   }
 }
