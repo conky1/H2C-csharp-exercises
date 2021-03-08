ac395a3b973a88352f4f71776bbc10c22d480105
conky-1 <petri.sipinen@centria.fi>
2021-03-08 16:58:54 +0200

    8.3.2021

--- Exercises/Part 1/Exercise 016/Program.cs
+++ Exercises/Part 1/Exercise 016/Program.cs
@@ -6,7 +6,12 @@ namespace exercise_16
   {
     public static void Main(string[] args)
     {
-
+        int day = 86400;
+        Console.Write("Give days: ");
+        int conv = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Days converted to seconds " + conv * day);
+        Console.ReadLine();
+        
       // Write your code here:
 
     }
