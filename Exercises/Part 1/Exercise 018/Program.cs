f53af5a5883565de8bc6e59168e7651cebe8c71c
conky-1 <petri.sipinen@centria.fi>
2021-03-08 17:12:02 +0200

    8.3.2021

--- Exercises/Part 1/Exercise 018/Program.cs
+++ Exercises/Part 1/Exercise 018/Program.cs
@@ -8,6 +8,14 @@ namespace exercise_18
     {
 
       // Write your code here:
+        Console.Write("Give number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Give second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Give thir number number: ");
+        int num3 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The sum is: " + (num1 + num2 + num3));
+        Console.ReadLine();
 
     }
   }
