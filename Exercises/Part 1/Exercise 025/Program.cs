b216c073614beabaa8321a818a02eb72489ef528
conky-1 <petri.sipinen@centria.fi>
2021-03-08 21:51:12 +0200

    8.3.2021

--- Exercises/Part 1/Exercise 025/Program.cs
+++ Exercises/Part 1/Exercise 025/Program.cs
@@ -8,6 +8,20 @@ namespace exercise_25
     {
       // Write your code here:
 
+      Console.Write("Give a number: ");
+      double num = Convert.ToDouble(Console.ReadLine());
+      
+      if (num == 1984){
+            
+            Console.WriteLine("Orwell");
+     
+                    }
+      else{
+            Console.WriteLine();
+            }
+
+      Console.ReadLine();
+
     }
   }
 }
