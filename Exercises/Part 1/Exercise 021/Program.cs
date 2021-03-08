52202ce648c34c02ad90a92aae8ae3427be5540f
conky-1 <petri.sipinen@centria.fi>
2021-03-08 17:43:52 +0200

    8.3.2021

--- Exercises/Part 1/Exercise 021/Program.cs
+++ Exercises/Part 1/Exercise 021/Program.cs
@@ -8,7 +8,12 @@ namespace exercise_21
     {
 
       // Write your code here:
-
+        Console.Write("Give number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Give second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("The average is: " + (num1 + num2)/2);
+        Console.ReadLine();
     }
   }
 }
