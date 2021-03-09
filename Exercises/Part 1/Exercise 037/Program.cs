eaeddac636266c33f3defc8c379381d835c3e6fc
conky-1 <petri.sipinen@centria.fi>
2021-03-09 14:29:02 +0200

    9.3.2021

--- Exercises/Part 1/Exercise 037/Program.cs
+++ Exercises/Part 1/Exercise 037/Program.cs
@@ -8,6 +8,19 @@ namespace exercise_37
     {
       int sum = 0;
       // Write your code here:
+      while (true){
+            Console.Write("Give a number: ");
+                int answer = Convert.ToInt32( Console.ReadLine());
+                if(answer == 0){
+                    Console.WriteLine("Total amount of numbers " + sum);
+                break;
+                }
+                else if(answer < 0 || answer > 0){
+                sum +=1;
+                Console.WriteLine();
+                }
+            }
+      Console.ReadLine();
 
     }
   }
