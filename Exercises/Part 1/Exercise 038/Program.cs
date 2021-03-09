239d0544c9757d314688c13f6a7078ec988d663a
conky-1 <petri.sipinen@centria.fi>
2021-03-09 14:34:57 +0200

    9.3.2021

--- Exercises/Part 1/Exercise 038/Program.cs
+++ Exercises/Part 1/Exercise 038/Program.cs
@@ -6,7 +6,21 @@ namespace exercise_38
   {
     public static void Main(string[] args)
     {
+      int sum = 0;
       // Write your code here:
+      while (true){
+            Console.Write("Give a number: ");
+                int answer = Convert.ToInt32( Console.ReadLine());
+                if(answer == 0){
+                    Console.WriteLine("Total amount of negative numbers " + sum);
+                break;
+                }
+                else if(answer < 0){
+                sum +=1;
+              
+                }
+            }
+      Console.ReadLine();
 
     }
   }
