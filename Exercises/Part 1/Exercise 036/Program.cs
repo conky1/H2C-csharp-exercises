5d538ef4e88342733be3d341012229bc2e9d097e
conky-1 <petri.sipinen@centria.fi>
2021-03-09 14:18:59 +0200

    9.3.2021

--- Exercises/Part 1/Exercise 036/Program.cs
+++ Exercises/Part 1/Exercise 036/Program.cs
@@ -7,7 +7,22 @@ namespace exercise_36
     public static void Main(string[] args)
     {
       // Write your code here:
-
+      while (true){
+            Console.Write("Give a number: ");
+                int answer = Convert.ToInt32( Console.ReadLine());
+                if(answer == 0){
+                    Console.WriteLine("Bye!");
+                break;
+                }
+                else if(answer < 0){
+                Console.WriteLine("That's negative");
+                Console.WriteLine();
+                }
+                else if(answer > 0){
+                Console.WriteLine(answer * answer);
+                }
+            }
+      Console.ReadLine();
     }
   }
 }
