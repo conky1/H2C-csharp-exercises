406ffccc5114f401a0b9c20bf8db32a66cac9189
conky-1 <petri.sipinen@centria.fi>
2021-03-09 14:48:01 +0200

    9.3.2021

--- Exercises/Part 1/Exercise 040/Program.cs
+++ Exercises/Part 1/Exercise 040/Program.cs
@@ -7,7 +7,23 @@ namespace exercise_40
     public static void Main(string[] args)
     {
 
+      int sum = 0;
+      int num = 0;
       // Write your code here:
+      while (true){
+            Console.Write("Give a number: ");
+                int answer = Convert.ToInt32( Console.ReadLine());
+                if(answer == 0){
+                    Console.WriteLine("Total sum of numbers " + sum +"\nTotal amount of numbers " + num);
+                break;
+                }
+                else if(answer < 0 || answer > 0){
+                sum +=answer;
+                num +=1;
+                
+                }
+            }
+      Console.ReadLine();
     }
   }
 }
