6240459addd54a4995a0fcc71ceeb1e0ae821d9e
conky-1 <petri.sipinen@centria.fi>
2021-03-09 11:23:30 +0200

    9.3.2021

--- Exercises/Part 1/Exercise 032/Program.cs
+++ Exercises/Part 1/Exercise 032/Program.cs
@@ -7,7 +7,17 @@ namespace exercise_32
     public static void Main(string[] args)
     {
       // Write your code here:
+        Console.Write("Speak friend and enter: \n");
+        string pass = Console.ReadLine();
+         
+        if(pass != "Mellon"){
+            Console.WriteLine("They've got a cave troll!");
 
+            }
+        else{
+            Console.WriteLine("Welcome friend!");
+            }
+        Console.ReadLine();
     }
   }
 }
