d964b2d6d686f6fbdcc0ed8fc513df176b92c583
conky-1 <petri.sipinen@centria.fi>
2021-03-09 17:28:25 +0200

    9.3.2021

--- Exercises/Part 2/Exercise 047/Program.cs
+++ Exercises/Part 2/Exercise 047/Program.cs
@@ -8,6 +8,15 @@ namespace exercise_47
     {
 
       // Write your code here:
+      Console.Write("Where to? ");
+            int value1 = Convert.ToInt32(Console.ReadLine());
+      Console.Write("Where from? ");
+            int value2 = Convert.ToInt32(Console.ReadLine());
+        while(value1 >= value2){
+            Console.WriteLine(value2);
+            value2++;
+            }
+        Console.ReadLine();
       
     }
   }
