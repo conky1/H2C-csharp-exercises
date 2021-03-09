4acbce9f989db3dbe11589002389108ad7afa818
conky-1 <petri.sipinen@centria.fi>
2021-03-09 14:00:08 +0200

    9.3.2021

--- Exercises/Part 1/Exercise 034/Program.cs
+++ Exercises/Part 1/Exercise 034/Program.cs
@@ -7,7 +7,19 @@ namespace exercise_34
     public static void Main(string[] args)
     {
       // Write your code here:
-
+      
+      while (true){
+            Console.Write("Do you want to quit? Yes/No: ");
+                string answer = Console.ReadLine();
+                if(answer.ToUpper() == "YES"){
+                    Console.WriteLine("Bye!");
+                break;
+                }
+            }
+         
+      
+            
+        Console.ReadLine();
     }
   }
 }
