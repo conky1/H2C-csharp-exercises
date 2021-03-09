995bcd2ddf8d20099290d3406290a695d633d508
conky-1 <petri.sipinen@centria.fi>
2021-03-09 23:37:38 +0200

    9.3.2021

--- Exercises/Part 2/Exercise 048/Program.cs
+++ Exercises/Part 2/Exercise 048/Program.cs
@@ -8,7 +8,36 @@ namespace exercise_48
     {
 
       // Write your code here:
- 
+      double sum = 0;
+      double amount = 0;
+      double even = 0;
+      double odd = 0;
+      
+      while(true){
+      Console.Write("Give numbers: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+            if(num == -1){
+             Console.WriteLine();
+             Console.WriteLine("Thx! Bye!\n" + "Sum: " + sum +"\nNumbers: " + amount);
+             Console.WriteLine("Average: " + sum/amount + "\nEven: " + even + "\nOdd: " + odd);
+             break;
+             
+            }
+            else if(num%2==0){
+                    sum+=num;
+                    amount++;
+                    even++;
+                    }
+            else{
+                sum+=num;
+                amount ++;
+                odd++;
+
+                }
+       
+      }
+       
+       Console.ReadLine();
     }
   }
 }
