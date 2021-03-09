c2f77887118e8df16172d287225a371fc9f70e9b
conky-1 <petri.sipinen@centria.fi>
2021-03-09 11:34:58 +0200

    9.3.2021

--- Exercises/Part 1/Exercise 033/Program.cs
+++ Exercises/Part 1/Exercise 033/Program.cs
@@ -7,7 +7,18 @@ namespace exercise_33
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.Write("Give the first string: ");
+      string first = Console.ReadLine();
+      Console.Write("Give the scond string: ");
+      string second = Console.ReadLine();
+            if(first == second){
+            Console.WriteLine("Echo!");
 
+            }
+            else{
+            Console.WriteLine("Nope!");
+            }
+      Console.ReadLine();
     }
   }
 }
