71e51e3bebb2de174c6812d13d65660363bc3d11
conky-1 <petri.sipinen@centria.fi>
2021-03-09 15:41:28 +0200

    9.3.2021

--- Exercises/Part 2/Exercise 044/Program.cs
+++ Exercises/Part 2/Exercise 044/Program.cs
@@ -8,7 +8,21 @@ namespace exercise_44
     {
 
       // Write your code here:
+      Console.Write("Give a number: ");
+            int first = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Give a second number: ");
+            int second = Convert.ToInt32(Console.ReadLine());
+            if(first > second){
+            Console.WriteLine(first + " is greater than " + second);
+            }
+            else if(first == second){
+            Console.WriteLine(first + " is equal to " + second);
+            }
+            else{
+            Console.WriteLine(first + " is less than " + second);
 
+            }
+            Console.ReadLine();
     }
   }
 }
