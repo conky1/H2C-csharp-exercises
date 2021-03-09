65d71a98f6fd6f081a5f9d66e9d1bf180109976b
conky-1 <petri.sipinen@centria.fi>
2021-03-09 15:51:30 +0200

    9.3.2021

--- Exercises/Part 2/Exercise 045/Program.cs
+++ Exercises/Part 2/Exercise 045/Program.cs
@@ -8,6 +8,14 @@ namespace exercise_45
     {
 
       // Write your code here:
+      int number = 0;
+        Console.Write("Give a number: ");
+            int value = Convert.ToInt32(Console.ReadLine());
+        while(number <= value){
+            Console.WriteLine(number);
+            number++;
+            }
+        Console.ReadLine();
 
     }
   }
