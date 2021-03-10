b0d63be6b351070807873e45366b6c80ee42282d
conky-1 <petri.sipinen@centria.fi>
2021-03-10 12:12:43 +0200

    10.3.2021

--- Exercises/Part 2/Exercise 049/Program.cs
+++ Exercises/Part 2/Exercise 049/Program.cs
@@ -7,6 +7,7 @@ namespace exercise_49
     public static void Main(string[] args)
     {
       // Call your method here:
+
       PrintPhrase();
         Console.ReadLine();
  
--- Exercises/Part 2/Exercise 050/Program.cs
+++ Exercises/Part 2/Exercise 050/Program.cs
@@ -7,11 +7,22 @@ namespace exercise_50
     public static void Main(string[] args)
     {
       // Call your method here:
- 
+      int i = 0;
+      Console.Write("How many times you want to print method: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+     while(i<num){
+                i++;
+       PrintPhrase();
+          
+     
+      
+                }
+        Console.ReadLine();
     }
 
     // Write your method here:
     public static void PrintPhrase() {
+            Console.WriteLine("In a hole in the ground there lived a method");
 
     }
   }
