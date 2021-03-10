18b1c2d187768fe5588f4efa1077a813f9ed731e
conky-1 <petri.sipinen@centria.fi>
2021-03-10 13:05:45 +0200

    10.3.2021

--- Exercises/Part 2/Exercise 052/Program.cs
+++ Exercises/Part 2/Exercise 052/Program.cs
@@ -7,10 +7,18 @@ namespace exercise_52
     public static void Main(String[] args)
     {
       // Call your method here:
-
+    printUntilToOne(10);
+            Console.ReadLine();
     }
 
     // Write your method here:
-
+    public static void printUntilToOne(int number){
+       int i = 0;
+            while(number > i){
+            Console.WriteLine(number);
+                number--;
+                
+            }
+    }
   }
 }
