58ece436a8cd7f95c56c3f85ec053d7cc94adb05
conky-1 <petri.sipinen@centria.fi>
2021-03-11 16:52:35 +0200

    11.3.2021

--- Exercises/Part 2/Exercise 058/Program.cs
+++ Exercises/Part 2/Exercise 058/Program.cs
@@ -8,9 +8,22 @@ namespace exercise_58
     {
       int answer = Smallest(2, 7);
       Console.WriteLine("Smallest: " + answer);
+            Console.ReadLine();
     }
 
     // Write your method here:
-    
+    public static int Smallest(int number1, int number2){
+            int smallest1 = number1;
+            int smallest2 = number2;
+            if(smallest1 < smallest2){
+                return smallest1;
+            }
+            else{
+                return smallest2;
+            }
+        
+     
+        
+        }
   }
 }
