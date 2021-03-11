7fb5777442a152379c65336f4231b66dfd52f0b5
conky-1 <petri.sipinen@centria.fi>
2021-03-11 17:03:00 +0200

    11.3.2021

--- Exercises/Part 2/Exercise 059/Program.cs
+++ Exercises/Part 2/Exercise 059/Program.cs
@@ -6,11 +6,27 @@ namespace exercise_59
   {
     public static void Main(String[] args)
     {
-      int answer = Greatest(2, 7, 3);
+      int answer = GrEatest(2, 7, 13);
       Console.WriteLine("Greatest: " + answer);
+            Console.ReadLine();
     }
 
     // Write your method here:
+    public static int GrEatest(int number1, int number2, int number3){
+        int great1 = number1;
+        int great2 = number2;
+        int great3 = number3;
+            if(great1 > great2 && great1 > great3){
+            return number1;
+            }
+            else if(great2>great1 && great2 > great3){
+            return number2;
+            }
+            else{
+            return number3;
+            }
 
+        
+        }
   }
 }
